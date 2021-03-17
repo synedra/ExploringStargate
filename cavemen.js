6e82925e96f00e269533738d9d901b5352c84a3f
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-03-17 07:40:37 -0700

    Finishing up cavemen.js

--- cavemen.js
+++ cavemen.js
@@ -1,22 +1,87 @@
-const { createRestClient } = require("@astrajs/rest");
-const { createDocClient } = require("@astrajs/collections");
-
-  
-  const docBasePath = "/api/rest/v2/keyspaces/workshop/collections/cavemen";
+const docBasePath = "/api/rest/v2/namespaces/workshop/collections/cavemen";
+  const restBasePath = "/api/rest/v2/keyspaces/workshop/"
+  const restSchemaPath = "/api/rest/v2/schemas/keyspaces/workshop/tables/"
   let data
+  let response
   let status
 
   async function runCalls () {
-    // create an Astra client for the Document API
-    const astraDocClient = await createClient({
+    const astraRest = require("@astrajs/rest");
+    const sleep = require('sleep');
+    const astraClient = await astraRest.createClient({
         astraDatabaseId: process.env.ASTRA_DB_ID,
         astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-        username: process.env.ASTRA_DB_USERNAME,
-        password: process.env.ASTRA_DB_PASSWORD,
+        authToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
     });
 
+    // REST API
+
+    // List my tables
+    response, status = await astraClient.get(restSchemaPath)
+    response, status = await astraClient.delete(restSchemaPath + "cavemen")
+    console.log(status)
+
+    // Create the cavemen table
+    data, status = await astraClient.post(restSchemaPath, {
+        name: "cavemen",
+        columnDefinitions: [
+          {
+            name: "firstname",
+            typeDefinition: "text",
+            static: false
+          },
+          {
+            name: "lastname",
+            typeDefinition: "text",
+            static: false
+          },
+              {
+                name: "occupation",
+                typeDefinition: "text"
+              }
+        ],
+        primaryKey: {
+          partitionKey: [
+            "lastname"
+          ],
+          clusteringKey: [
+            "firstname"
+          ]
+        }
+      });
+
+        // Rest a bit so the table gets created
+        sleep.msleep(5000);
+
+        response = await astraClient.post(restBasePath + "cavemen", {
+        firstname: "Fred",
+        lastname: "Flintstone"
+        });
+    
+    
+    if (status.status==201) {
+        console.log("Created Fred in the cavemen collection")
+    } 
+
+    // check our tables again
+    let query = '{\"lastname\":{\"$in\":[\"Rubble\",\"Flintstone\"]}}'
+     response = await astraClient.get(restBasePath+"cavemen?where=" + query);
+     console.log(response)
+
+    // create a new user
+     response = await astraClient.post(restBasePath+"cavemen", {
+        firstname: "Barney",
+        lastname: "Rubble"
+    });
+
+    if (response.status==201) {
+        console.log("Created Barney in the cavemen collection")
+    }
+
+    
+    // Document API
     // create a new user without a document id
-    data, status = await astraDocClient.post(DocBasePath, {
+    data, status = await astraClient.post(docBasePath, {
         firstname: "Fred",
         lastname: "Flintstone"
     });
@@ -24,10 +89,10 @@ const { createDocClient } = require("@astrajs/collections");
     if (status==201) {
         console.log("Created Fred in the cavemen collection")
         let fredID = data.documentId
-    }
+    } 
 
     // search a collection of documents
-     data, status = await astraClient.get(basePath, {
+     data, status = await astraClient.get(docBasePath, {
         params: {
             where: {
                 firstname: { $eq: "Fred" }
@@ -36,7 +101,7 @@ const { createDocClient } = require("@astrajs/collections");
     });
 
     // create a new user with a document id
-     data, status = await astraClient.put(`${DocBasePath}/BarneyRubble`, {
+     data, status = await astraClient.put(`${docBasePath}/BarneyRubble`, {
         firstname: "Barney",
         lastname: "Rubble"
     });
@@ -47,23 +112,77 @@ const { createDocClient } = require("@astrajs/collections");
 
    
     // get a single user by document id
-     data, status = await astraDocClient.get(`${docBasePath}/BarneyRubble`);
+     data, status = await astraClient.get(`${docBasePath}/BarneyRubble`);
 
     // get a subdocument by path
-     data, status = await astraDocClient.get(`${docBasePath}/BarneyRubble/firstname`);
+     data, status = await astraClient.get(`${docBasePath}/BarneyRubble/firstname`);
 
     // create a user subdocument
-     data, status = await astraDocClient.put(`${docBasePath}/BarneyRubble/occupation`, {
+     data, status = await astraClient.put(`${docBasePath}/BarneyRubble/occupation`, {
         title: "Fred's Friend",
     });
 
     // partially update user
-     data, status = await astraDocClient.patch(`${docBasePath}/fredID`, {
+     data, status = await astraClient.patch(`${docBasePath}/fredID`, {
         firstname: "Wilma",
     });
 
     // delete a user
-     data, status = await astraDocClient.delete(`${docBasePath}/fredID`);
+     data, status = await astraClient.delete(`${docBasePath}/fredID`);
+  
+    
+
+    // create a new user without a document id
+    data, status = await astraClient.post(docBasePath, {
+        firstname: "Fred",
+        lastname: "Flintstone"
+    });
+
+    if (status==201) {
+        console.log("Created Fred in the cavemen collection")
+        let fredID = data.documentId
+    }
+
+    // search a collection of documents
+     data, status = await astraClient.get(docBasePath, {
+        params: {
+            where: {
+                firstname: { $eq: "Fred" }
+            }
+        }
+    });
+
+    // create a new user with a document id
+     data, status = await astraClient.put(`${docBasePath}/BarneyRubble`, {
+        firstname: "Barney",
+        lastname: "Rubble"
+    });
+
+    if (status==201) {
+        console.log("Created Barney in the cavemen collection")
+    }
+
+   
+    // get a single user by document id
+     data, status = await astraClient.get(`${docBasePath}/BarneyRubble`);
+
+    // get a subdocument by path
+     data, status = await astraClient.get(`${docBasePath}/BarneyRubble/firstname`);
+
+    // create a user subdocument
+     data, status = await astraClient.put(`${docBasePath}/BarneyRubble/occupation`, {
+        title: "Fred's Friend",
+    });
+
+    // partially update user
+     data, status = await astraClient.patch(`${docBasePath}/fredID`, {
+        firstname: "Wilma",
+    });
+
+    // delete a user
+     data, status = await astraClient.delete(`${docBasePath}/fredID`);
+
+
   }
 
 runCalls();
